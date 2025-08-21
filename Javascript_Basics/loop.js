1ca096e3338dede1aaa42e55b6ca814cddf6dfde
anumHub <anumsfo@gmail.com>
2025-08-21 16:41:40 -0700

    loops concepts

---
+++ Javascript_Basics/loop.js
@@ -0,0 +1,16 @@
+const flag = true
+
+if(flag)
+{
+    console.log("condition satisfied")
+}
+else
+{
+    console.log("condition not satisfied")
+}
+let i=0
+while(i<10)
+{
+    i=i+1
+    console.log(i)
+}
